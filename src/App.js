4b36dd3adfc627ab7697d1758397c77bcd00879b
Elian Gutierrez <eliangu3600103@gmail.com>
2018-11-11 14:12:20 -0500

    Add and setup React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const routes = [
+	{
+		path: '/',
+		exact: true,
+		component: Home
+	}
+];
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				{routes.map((route, index) => (
+					<Route {...route} key={index} />
+				))}
+			</div>
+		</Router>
+	);
 }
 
 export default App;

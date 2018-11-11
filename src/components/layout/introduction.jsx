7124c2c587f4a528d9a308e879e852c76a8d8103
Elian Gutierrez <eliangu3600103@gmail.com>
2018-11-11 16:50:46 -0500

    Add introduction and styled component

---
+++ src/components/layout/introduction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './intro.css';
+
+function Introduction() {
+	return (
+		<div className="introduction">
+			<h1 className="app__title">SharoIT</h1>
+			<p className="app__description">A free and open source code sharing tool</p>
+		</div>
+	);
+}
+
+export default Introduction;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// components
+import Introduction from '../components/layout/introduction';
+
+const Wrapper = styled.section`
+	background: papayawhip;
+	height: 100vh;
+	width: 100%;
+	padding: 50px 0;
+`;
 
 function Home() {
-	return <h1>Hello World</h1>;
+	return (
+		<Wrapper>
+			<Introduction />
+		</Wrapper>
+	);
 }
 
 export default Home;
